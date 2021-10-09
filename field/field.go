68d7df51d2b4140df66fcf2266700518a49c27e7
Brendan Jackman <bhenryj0117@gmail.com>
2021-10-09 19:02:21 +0200

    Add basic bilinear interpolation to SqareGrid

--- field/field.go
+++ field/field.go
@@ -1,6 +1,9 @@
 package field
 
-import "egosystem/space"
+import (
+	"egosystem/space"
+	"log"
+)
 
 type SquareGrid struct {
 	nodes      [][]int8
@@ -25,6 +28,48 @@ func (g *SquareGrid) SetUniform(value int8) {
 	}
 }
 
+type index2D struct{ x, y int }
+
+func (i index2D) InSquare(size int) bool {
+	return i.x >= 0 && i.x < size && i.y >= 0 && i.y < size
+}
+
+// Given a unit square field with nodes zxy at each of the four corners, do
+// bilinear interpolation to get the value at x,y inside that square.
+// https://blogs.sas.com/content/iml/2020/05/18/what-is-bilinear-interpolation.html
+func bilinearInterpUnit(z00, z01, z10, z11 int8, x, y float32) float32 {
+	return float32(z00)*(1-x)*(1-y) +
+		float32(z10)*x*(1-y) +
+		float32(z01)*(1-x)*y +
+		float32(z11)*x*y
+}
+
 func (g *SquareGrid) Value(point space.Point) float32 {
-	return float32(g.nodes[0][0]) // lol
+	if g.resolution == space.Distance(0) {
+		log.Panic("what")
+	}
+
+	// Index of originwards node of the square that point is in
+	origin := index2D{
+		x: point.X.Div(g.resolution),
+		y: point.Y.Div(g.resolution),
+	}
+	if !origin.InSquare(g.size) {
+		// TODO: Handle this, log non-fatally but only if it's outside by a non-negligible margin
+		log.Fatalf("Point %v not within square grid of size %v@%v", point, g.size, g.resolution)
+	}
+
+	log.Printf("%+v", origin)
+	// Get position of point within the square, treating the square's size as unit.
+	res := float32(g.resolution.Centimeters())
+	squareX := float32(point.X%g.resolution) / res
+	squareY := float32(point.Y%g.resolution) / res
+
+	return bilinearInterpUnit(
+		g.nodes[origin.x+0][origin.y+0],
+		g.nodes[origin.x+0][origin.y+1],
+		g.nodes[origin.x+1][origin.y+0],
+		g.nodes[origin.x+1][origin.y+1],
+		squareX, squareY,
+	)
 }
--- field/field_test.go
+++ field/field_test.go
@@ -3,6 +3,9 @@ package field
 import (
 	"egosystem/space"
 	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
 )
 
 func TestSquareGridUniform(t *testing.T) {
@@ -26,6 +29,45 @@ func TestSquareGridUniform(t *testing.T) {
 				t.Errorf("After SetUniform(%v), Value(%v) returned %v, want %v",
 					value, point, got, value)
 			}
+		}
+	}
+}
+
+func TestSquareGridInterpolation(t *testing.T) {
+	grid := SquareGrid{
+		// Warning: X axis is veritical in this literal!
+		nodes: [][]int8{
+			{0, 100, 0},
+			{-70, 100, -100},
+			{0, -50, 0},
+		},
+		size:       3,
+		resolution: space.Meter,
+	}
+	for point, want := range map[space.Point]float32{
+		// Corner node
+		{}: 0.0,
+		// Edge node
+		{X: 1 * space.Meter, Y: 0}: -70.0,
+		// Originwards edge of grid
+		{X: 50 * space.Centimeter, Y: 0}:  -35.0,
+		{X: 0, Y: 125 * space.Centimeter}: 75.0,
+		// Non-originwards edge of grid
+		// TODO: {X: 2 * space.Meter, Y: 10 * space.Centimeter}: -5.0,
+		// TODO: OOB in one dimension
+		// TODO: OOB in both dimensions
+		// Node
+		{X: 1 * space.Meter, Y: 1 * space.Meter}: 100.0,
+		// Edge
+		{X: 1 * space.Meter, Y: 170 * space.Centimeter}: -40.0,
+		// In the middle
+		// Calculated by
+		// https://www.ajdesigner.com/phpinterpolation/bilinear_interpolation_equation.php
+		{X: 145 * space.Centimeter, Y: 163 * space.Centimeter}: -22.625,
+	} {
+		got := grid.Value(point)
+		if diff := cmp.Diff(got, want, cmpopts.EquateApprox(0, 0.000001)); diff != "" {
+			t.Errorf("Value(%v) returned %v, want %v", point, got, want)
 		}
 	}
 }
