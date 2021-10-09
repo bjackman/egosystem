88526c349b7a6031f15a1b91954e5faa6716a7a0
Brendan Jackman <bhenryj0117@gmail.com>
2021-10-09 18:59:00 +0200

    Rename SquareGrid.values to nodes

--- field/field.go
+++ field/field.go
@@ -3,16 +3,16 @@ package field
 import "egosystem/space"
 
 type SquareGrid struct {
-	values     [][]int8
+	nodes      [][]int8
 	size       int
 	resolution space.Distance
 }
 
 func NewSquareGrid(size int, resolution space.Distance) SquareGrid {
-	g := SquareGrid{size: size}
-	g.values = make([][]int8, size)
-	for i := range g.values {
-		g.values[i] = make([]int8, size)
+	g := SquareGrid{size: size, resolution: resolution}
+	g.nodes = make([][]int8, size)
+	for i := range g.nodes {
+		g.nodes[i] = make([]int8, size)
 	}
 	return g
 }
@@ -20,11 +20,11 @@ func NewSquareGrid(size int, resolution space.Distance) SquareGrid {
 func (g *SquareGrid) SetUniform(value int8) {
 	for x := 0; x < g.size; x++ {
 		for y := 0; y < g.size; y++ {
-			g.values[x][y] = value
+			g.nodes[x][y] = value
 		}
 	}
 }
 
 func (g *SquareGrid) Value(point space.Point) float32 {
-	return float32(g.values[0][0]) // lol
+	return float32(g.nodes[0][0]) // lol
 }
